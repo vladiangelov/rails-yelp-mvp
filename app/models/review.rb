eff1eb5dfeb59bc30c4154bfe9947f452c327a69
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-01-21 21:32:04 +0000

    Basic CRUD implemented

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5],
+    message: "%{value} is not a valid rating" }
+  validates :rating, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rest1 = Restaurant.new(name: "Jamie's", address: "Shoreditch", category: "italian")
+rest1.save
+rest2 = Restaurant.new(name: "Wagamama", address: "Horsham", category: "japanese")
+rest2.save
+rest3 = Restaurant.new(name: "Cafe Rouge", address: "Greenwich", category: "french")
+rest3.save
+rest4 = Restaurant.new(name: "Pret", address: "City", category: "belgian")
+rest4.save
+rest5 = Restaurant.new(name: "Beijing", address: "Shoreditch", category: "chinese")
+rest5.save
+rest6 = Restaurant.new(name: "Chiana Peking", address: "Crawley", category: "chinese")
+rest6.save
